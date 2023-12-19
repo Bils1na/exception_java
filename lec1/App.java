a6402903865153f77aaff6e4b736288c602b4fd1
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-12-19 11:20:29 +0000

    fix

---
+++ lec1/App.java
@@ -0,0 +1,14 @@
+package lec1;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            // throw ;
+        }
+        return a / b;
+    }
+}
