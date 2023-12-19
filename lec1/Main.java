762db0b714b30f8996984ebdc7200eb13c2837b1
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-12-19 11:05:55 +0000

    added lec1

--- lec1/Main.java
+++ lec1/Main.java
@@ -1,13 +1,40 @@
 package lec1;
 
-/**
- * Main
- */
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println(getFileSize(new File("123")));
+        System.out.println(divide(10, 0));
+
+        a();
     }
 
-    
+    public static void a() {
+        b();
+    }
+
+    public static void b() {
+        c();
+    }
+
+    public static void c() {
+        int[] nums = new int[10];
+        System.out.println(nums[1000]);
+    }
+
+    public static int divide(int a1, int a2) {
+        if (a2 == 0) {
+            return -1;
+        }
+        return a1 / a2;
+    }
+
+    public static long getFileSize(File file) {
+        if (!file.exists()) {
+            return -1L;
+        }
+        return file.length();
+    }
 }
