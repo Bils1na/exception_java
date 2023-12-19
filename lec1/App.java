603202732c030f4f54007b4c2cb712136af8ee0b
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-12-19 11:24:54 +0000

    finished lec1

--- lec1/App.java
+++ lec1/App.java
@@ -2,12 +2,12 @@ package lec1;
 
 public class App {
     public static void main(String[] args) {
-        
+        divide(10, 0);
     }
 
     public static int divide(int a, int b) {
         if (b == 0) {
-            // throw ;
+            throw new RuntimeException("Divide by zero not permited");
         }
         return a / b;
     }
