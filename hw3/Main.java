5535ee9af77029aca7afb699fc45c5980392646e
bilsina <bils1n@icloud.com>
2023-12-21 20:54:53 +0300

    fixed hw3

--- hw3/Main.java
+++ hw3/Main.java
@@ -6,13 +6,13 @@ import hw3.exception.NumberSizeException;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
         DataHandler dataHandler = new DataHandler();
         try {
             dataHandler.addNewUser();
-            System.out.println(dataHandler.getUserData());
             try {
                 writeUserData(dataHandler, Path.of(String.format("./hw3/%s.txt", dataHandler.getUserSurname())));
             } catch (IOException e) {
@@ -29,21 +29,32 @@ public class Main {
 
     public static void writeUserData(DataHandler dh, Path pathWrite) throws IOException {
         try (FileWriter writer = new FileWriter(String.valueOf(pathWrite), true)) {
-            writer.write(dh.getUserData() + "\n");
+            if (checkUserExisting(pathWrite, dh)) {
+                writer.write(dh.getUserData() + "\n");
+            } else {
+                System.out.println("Такой пользователь уже сущетсвует");
+            }
         } catch (IOException e) {
             System.out.println("Ошибка при работе с файлом");
         }
     }
 
-    private static void checkPhonebook (Path pathRead, DataHandler dh) throws FileNotFoundException {
+    private static boolean checkUserExisting (Path pathRead, DataHandler dh) throws FileNotFoundException {
         File file = new File(String.valueOf(pathRead));
+        boolean isExist = true;
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-
+            Iterator<String> iterator = bufferedReader.lines().iterator();
+            while (iterator.hasNext()) {
+                String string = iterator.next();
+                if (string.equals(dh.getUserData())) {
+                    isExist = false;
+                }
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         }
-
+        return isExist;
     }
 
 }
