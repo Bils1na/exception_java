cfd27737e0bbec9422744f940dba8ab570b62558
bilsina <bils1n@icloud.com>
2023-12-21 20:58:03 +0300

    fixed hw3

---
+++ hw3/FileHandler.java
@@ -0,0 +1,38 @@
+package hw3;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.Iterator;
+
+public class FileHandler {
+
+    public static void writeUserData(DataHandler dh, Path pathWrite) throws IOException {
+        try (FileWriter writer = new FileWriter(String.valueOf(pathWrite), true)) {
+            if (checkUserExisting(pathWrite, dh)) {
+                writer.write(dh.getUserData() + "\n");
+            } else {
+                System.out.println("Такой пользователь уже сущетсвует");
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при работе с файлом");
+        }
+    }
+
+    private static boolean checkUserExisting (Path pathRead, DataHandler dh) throws FileNotFoundException {
+        File file = new File(String.valueOf(pathRead));
+        boolean isExist = true;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            Iterator<String> iterator = bufferedReader.lines().iterator();
+            while (iterator.hasNext()) {
+                String string = iterator.next();
+                if (string.equals(dh.getUserData())) {
+                    isExist = false;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        return isExist;
+    }
+}
--- hw3/Main.java
+++ hw3/Main.java
@@ -14,7 +14,7 @@ public class Main {
         try {
             dataHandler.addNewUser();
             try {
-                writeUserData(dataHandler, Path.of(String.format("./hw3/%s.txt", dataHandler.getUserSurname())));
+                FileHandler.writeUserData(dataHandler, Path.of(String.format("./hw3/%s.txt", dataHandler.getUserSurname())));
             } catch (IOException e) {
                 System.out.println("Ошибка в работе с файлом");
             }
@@ -25,36 +25,6 @@ public class Main {
         }
 
 
-    }
-
-    public static void writeUserData(DataHandler dh, Path pathWrite) throws IOException {
-        try (FileWriter writer = new FileWriter(String.valueOf(pathWrite), true)) {
-            if (checkUserExisting(pathWrite, dh)) {
-                writer.write(dh.getUserData() + "\n");
-            } else {
-                System.out.println("Такой пользователь уже сущетсвует");
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка при работе с файлом");
-        }
-    }
-
-    private static boolean checkUserExisting (Path pathRead, DataHandler dh) throws FileNotFoundException {
-        File file = new File(String.valueOf(pathRead));
-        boolean isExist = true;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            Iterator<String> iterator = bufferedReader.lines().iterator();
-            while (iterator.hasNext()) {
-                String string = iterator.next();
-                if (string.equals(dh.getUserData())) {
-                    isExist = false;
-                }
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден");
-        }
-        return isExist;
     }
 
 }
